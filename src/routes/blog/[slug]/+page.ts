7c30c13083eafefa3f44aa87bab2d6eb6e0577fb
José Alisson Oliveira Valério <joseaov@jfal.jus.br>
2023-03-18 13:55:08 -0300

    Add dynamic Route

---
+++ src/routes/blog/[slug]/+page.ts
@@ -0,0 +1,21 @@
+import { error } from '@sveltejs/kit';
+
+interface Params {
+  slug: string;
+}
+
+interface Result {
+  title: string;
+  content: string;
+}
+
+export function load( { params }: { params: Params }) : Result  {
+  if (params.slug === 'hello-world') {
+    return {
+      title: 'Hello world!',
+      content: 'Welcome to our blog.'
+    }
+  }
+
+  throw error(404, 'Not found');
+}
