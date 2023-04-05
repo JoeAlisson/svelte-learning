bf5627e387f4b21622ae33492207bfd08db5689e
Alisson Valério <joe.alisson@gmail.com>
2023-04-05 17:08:47 -0300

    Default Actions

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,11 @@
+<form method="POST">
+  <label>
+    Email
+    <input type="email" name="email" />
+  </label>
+  <label>
+    Password
+    <input type="password" name="password" />
+  </label>
+  <button>Log in</button>
+</form>
